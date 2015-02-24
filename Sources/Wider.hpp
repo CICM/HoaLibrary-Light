2e57470dd5eb5fc3e27fa8a58aa7378ad5701921
eliott paris <eliottparis@gmail.com>
2015-02-24 02:04:56 +0100

    date up .. update

--- Sources/Wider.hpp
+++ Sources/Wider.hpp
@@ -1,5 +1,5 @@
 /*
-// Copyright (c) 2012-2014 Eliott Paris, Julien Colafrancesco & Pierre Guillot, CICM, Universite Paris 8.
+// Copyright (c) 2012-2015 Eliott Paris, Julien Colafrancesco & Pierre Guillot, CICM, Universite Paris 8.
 // For information on usage and redistribution, and for a DISCLAIMER OF ALL
 // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
 */
