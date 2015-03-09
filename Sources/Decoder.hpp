7609ac41957a232bd2c84c858210f23bb33873de
pierreguillot <guillotpierre6@gmail.com>
2015-03-09 15:37:58 +0100

    start binaural

--- Sources/Decoder.hpp
+++ Sources/Decoder.hpp
@@ -118,6 +118,60 @@ namespace hoa
             }
         }
     };
+    
+    //! The ambisonic decoder.
+    /** The decoder should be used to decode an ambisonic sound field for a set a channels on a circle depending on a decomposition order or for headphones.
+     */
+    template <Dimension D, typename T> class Binaural;
+    
+    template <typename T> class Binaural<Hoa2d, T> : public Encoder<Hoa2d, T>, public Planewave<Hoa2d, T>::Processor
+    {
+    private:
+        T*  m_matrix;
+    public:
+        
+        //! The regular constructor.
+        /**	The regular constructor allocates and initialize the decoding matrix depending of a decomposition order and a number of channels. The order must be at least 1 and the number of channels must be at least the number of harmonics.
+         @param     order				The order
+         @param     numberOfPlanewaves     The number of channels.
+         */
+        Binaural(const ulong order, const ulong numberOfPlanewaves) noexcept :
+        Encoder<Hoa2d, T>(order),
+        Planewave<Hoa2d, T>::Processor(numberOfPlanewaves)
+        {
+            m_matrix = new T[Planewave<Hoa2d, T>::Processor::getNumberOfPlanewaves()*Encoder<Hoa2d, T>::getNumberOfHarmonics()];
+            computeMatrix();
+        }
+        
+        //! The destructor.
+        /** The destructor free the memory.
+         */
+        ~Binaural()
+        {
+            delete [] m_matrix;
+        }
+        
+        //! This method performs the decoding.
+        /**	You should use this method for in-place or not-in-place processing and performs the regular decoding sample by sample. The inputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics and the outputs array contains the channels samples and the minimym size must be the number of channels.
+         @param     inputs  The input array that contains the samples of the harmonics.
+         @param     outputs The output array that contains samples destinated to channels.
+         */
+        inline void process(const T* inputs, T* outputs) const noexcept
+        {
+            Signal<T>::matrix_vector_mul(Encoder<Hoa2d, T>::getNumberOfHarmonics(), Planewave<Hoa2d, T>::Processor::getNumberOfPlanewaves(), inputs, m_matrix, outputs);
+        }
+        
+        void computeMatrix() noexcept
+        {
+            const T factor = 1. / (T)(Encoder<Hoa2d, T>::getDecompositionOrder() + 1.);
+            for(ulong i = 0; i < Planewave<Hoa2d, T>::Processor::getNumberOfPlanewaves(); i++)
+            {
+                Encoder<Hoa2d, T>::setAzimuth(Planewave<Hoa2d, T>::Processor::getPlanewaveAzimuthRotated(i));
+                Encoder<Hoa2d, T>::process(&factor, m_matrix + i * Encoder<Hoa2d, T>::getNumberOfHarmonics());
+                m_matrix[i * Encoder<Hoa2d, T>::getNumberOfHarmonics()] = factor * 0.5;
+            }
+        }
+    };
 }
 
 #endif
