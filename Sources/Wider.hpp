f447a2137f394d35022af9cd45b5bc8226373160
pierreguillot <guillotpierre6@gmail.com>
2015-03-19 13:49:56 +0100

    Wider doc

--- Sources/Wider.hpp
+++ Sources/Wider.hpp
@@ -14,7 +14,7 @@ namespace hoa
     //! The wider class wides the diffusion of the sound field in the harmonics domain.
     /** The wider simulates fractional orders to diffuse a sound field from omni directional when the widening factor is \f$0\f$ to the most directional when the widening factor is \f$1\f$.
      */
-    template <Dimension D, typename T> class Wider : public Harmonic<D, T>::Processor
+    template <Dimension D, typename T> class Wider : public Processor< Harmonic<D, T> >
     {
     public:
         
@@ -27,29 +27,40 @@ namespace hoa
         //! The wider destructor.
         /**	The wider destructor free the memory.
          */
-        virtual ~Wider() noexcept = 0;
+        virtual ~Wider() noexcept;
         
         //! This method set the widening value.
         /**	The the widening value is between \f$0\f$ and \f$1\f$. At \f$0\f$, the sound field is omni directional and at \f$1\f$ the sound field is instact.
          @param     radius   The radius.
          @see       setAzimuth()
          */
-        virtual void setWidening(const T radius) noexcept = 0;
+        virtual void setWidening(const T radius) noexcept;
         
         //! Get the the widening value.
         /** The method returns the the widening value.
          @return     The widening value.
          */
-        virtual T getWidening() const noexcept = 0;
+        virtual T getWidening() const noexcept;
         
         //! This method perform the widening.
-        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
+        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics. \n
+         If \f$l = 0\f$
+         \f[h'_{0,0}(x) = (g_x \times N + 1) \times h_{l,m}\f]
+         else
+         \f[h'_{l,m}(x) = \frac{(g_x \times (N - 1) + 1) (\cos{(\min{(0, \max{(a_x \times l, \pi)})})} + 1)}{2} \times h_{l,m}\f]
+         with 
+         \f[a_x = (1 - x)\pi\f]
+         and
+         \f[g_x = \frac{\sin{(a_x - \frac{\pi}{2} + 1)}}{2}\f]
+         with \f$x \in{[0, 1]} \f$ the widening factor, \f$N\f$ the order of decomposition, \f$l\f$ the degree and \f$m\f$ the order.
          @param     inputs	The input array.
          @param     outputs The output array.
          */
-        virtual void process(const T* inputs, T* outputs) const noexcept = 0;
+        virtual void process(const T* inputs, T* outputs) const noexcept;
 
     };
+    
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
     
     template <typename T> class Wider<Hoa2d, T> : public Processor< Harmonic<Hoa2d, T> >
     {
@@ -193,6 +204,8 @@ namespace hoa
             }
         }
     };
+    
+#endif
 }
 
 #endif
