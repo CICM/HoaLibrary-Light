34473bb5d84e580aa8e7cb02e77f87f337835510
pierreguillot <guillotpierre6@gmail.com>
2015-05-26 17:53:42 +0200

    Signal class can avoid cblas

--- Sources/Signal.hpp
+++ Sources/Signal.hpp
@@ -76,6 +76,14 @@ namespace hoa
         @param   destVector   The destination vector.
          */
         virtual inline void vector_copy(const ulong vectorsize, const T* source, T* dest) = 0;
+        
+        //! Copy a vector into an other.
+        /** Copy a vector into an other.
+         @param   vectorSize   The size of the vector.
+         @param   srcVector    The source vector.
+         @param   destVector   The destination vector.
+         */
+        static inline void vector_copy(const ulong vectorsize, const double* source, const ulong incs, double* dest, const ulong incd) = 0;
 
         //! Add a vector to an other.
         /** Add a vector to an other value by value.
@@ -118,7 +126,15 @@ namespace hoa
          */
         static inline void matrix_vector_mul(const ulong inputsize, const ulong outputsize, const float* vector, const float* matrix, float* outputs)
         {
+#ifdef HOA_USE_CBLAS
             cblas_sgemv(CblasRowMajor, CblasNoTrans, (const int)outputsize, (const int)inputsize, 1.f, matrix, (const int)inputsize, vector, 1, 0.f, outputs, 1);
+#else
+            outputs[0] = vectors_dot_product(inputsize, vector, matrix);
+            for(ulong i = 1ul; i < outputsize; i++)
+            {
+                outputs[i] = vectors_dot_product(inputsize, vector, matrix+i*inputsize);
+            }
+#endif
         }
 
         //! Multiply a matrix by a matrix.
@@ -132,7 +148,27 @@ namespace hoa
          */
         static inline void matrix_matrix_mul(const ulong innrow, const ulong outcolumn, const ulong incolumn, const float* in1, const float* in2, float* out)
         {
+#ifdef HOA_USE_CBLAS
             cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, (const int)innrow, (const int)outcolumn, (const int)incolumn, 1., in1, incolumn, in2, outcolumn, 0., out,  outcolumn);
+#else
+            const float* a = in1;
+            for(ulong c = 0; c < innrow; c++)
+            {
+                const float* b = in2;
+                for(ulong d = 0; d < outcolumn; d++)
+                {
+                    *out = 0.f;
+                    const float* a2 = a;
+                    for(ulong k = 0; k < incolumn; k++)
+                    {
+                        *out += *(a2++) * *(b + k * outcolumn);
+                    }
+                    ++out;
+                    ++b;
+                }
+                a += incolumn;
+            }
+#endif
         }
 
         //! Get the max value of a vector.
@@ -147,8 +183,19 @@ namespace hoa
             float result;
             vDSP_maxmgv(vector, 1, &result, vectorsize);
             return result;
-#else
+#elif HOA_USE_CBLAS
             return vector[cblas_isamax((const int)vectorsize, vector, 1)];
+#else
+            float max = fabs(vector[0]);
+            for(ulong i = 1ul; i < vectorsize; i++)
+            {
+                const double temp = fabs(vector[1]);
+                if(temp > max)
+                {
+                    max = temp;
+                }
+            }
+            return max;
 #endif
         }
 
@@ -160,7 +207,16 @@ namespace hoa
          */
         static inline float vector_sum(const ulong vectorsize, const float* vector)
         {
+#ifdef HOA_USE_CBLAS
             return cblas_sasum(vectorsize, vector, 1);
+#else
+            float sum = fabs(vector[0]);
+            for(ulong i = 1ul; i < vectorsize; i++)
+            {
+                sum += fabs(vector[i]);
+            }
+            return sum;
+#endif
         }
 
         //! Multiply each element of a vector by a factor.
@@ -171,7 +227,14 @@ namespace hoa
          */
         static inline void vector_scale(const ulong vectorsize, const float value, float* vector)
         {
+#ifdef HOA_USE_CBLAS
             cblas_sscal((const int)vectorsize, value, vector, 1.);
+#else
+            for(ulong i = 0ul; i < vectorsize; i++)
+            {
+                vector[i] += value;
+            }
+#endif
         }
 
         //! Clear a vector.
@@ -196,7 +259,34 @@ namespace hoa
          */
         static inline void vector_copy(const ulong vectorsize, const float* source, float* dest)
         {
+#ifdef HOA_USE_CBLAS
             cblas_scopy(vectorsize, source, 1, dest, 1);
+#else
+            memcpy(dest, source, vectorsize * sizeof(float));
+#endif
+        }
+        
+        //! Copy a vector into an other.
+        /** Copy a vector into an other.
+         @param   vectorSize   The size of the vector.
+         @param   srcVector    The source vector.
+         @param   destVector   The destination vector.
+         */
+        static inline void vector_copy(const ulong vectorsize, const float* source, const ulong incs, float* dest, const ulong incd)
+        {
+#ifdef HOA_USE_CBLAS
+            cblas_scopy(vectorsize, source, incs, dest, incd);
+#else
+            ulong is = incs;
+            ulong id = incd;
+            dest[0] = source[0];
+            for(ulong i = 1; i < vectorsize; i++)
+            {
+                dest[id] = source[is];
+                is += incs;
+                id += incd;
+            }
+#endif
         }
 
         //! Add a vector to an other.
@@ -207,7 +297,14 @@ namespace hoa
          */
         static inline void vector_add(const ulong vectorsize, const float* source, float* dest)
         {
+#ifdef HOA_USE_CBLAS
             cblas_saxpy(vectorsize, 1., source, 1, dest, 1);
+#else
+            for(ulong i = 0ul; i < vectorsize; i++)
+            {
+                dest[i] += source[i];
+            }
+#endif
         }
 
         //! Add a vector to an other.
@@ -220,7 +317,20 @@ namespace hoa
          */
         static inline void vector_add(const ulong vectorsize, const float* source, const ulong incs, float* dest, const ulong incd)
         {
+#ifdef HOA_USE_CBLAS
             cblas_saxpy(vectorsize, 1., source, incs, dest, incd);
+#else
+            ulong is = incs;
+            ulong id = incd;
+            dest[0] += source[0];
+            for(ulong i = 1; i < vectorsize; i++)
+            {
+                dest[id] += source[is];
+                is += incs;
+                id += incd;
+            }
+#endif
+            
         }
 
         //! Compute the dot product of two vectors.
@@ -232,7 +342,17 @@ namespace hoa
          */
         static inline float vectors_dot_product(const ulong vectorsize, const float* vector1, const float* vector2)
         {
+#ifdef HOA_USE_CBLAS
             return cblas_sdot(vectorsize, vector1, 1, vector2, 1);
+#else
+            float sum = vector1[0] * vector2[0];
+            for(ulong i = 1ul; i < vectorsize; i++)
+            {
+                sum += vector1[i] * vector2[i];
+            }
+            return sum;
+#endif
+            
         }
     };
 
@@ -249,7 +369,15 @@ namespace hoa
          */
         static inline void matrix_vector_mul(const ulong inputsize, const ulong outputsize, const double* vector, const double* matrix, double* outputs)
         {
+#ifdef HOA_USE_CBLAS
             cblas_dgemv(CblasRowMajor, CblasNoTrans, (const int)outputsize, (const int)inputsize, 1., matrix, (const int)inputsize, vector, 1, 0., outputs, 1);
+#else
+            outputs[0] = vectors_dot_product(inputsize, vector, matrix);
+            for(ulong i = 1ul; i < outputsize; i++)
+            {
+                outputs[i] = vectors_dot_product(inputsize, vector, matrix+i*inputsize);
+            }
+#endif
         }
 
         //! Multiply a matrix by a matrix.
@@ -263,7 +391,27 @@ namespace hoa
          */
         static inline void matrix_matrix_mul(const ulong innrow, const ulong outcolumn, const ulong incolumn, const double* in1, const double* in2, double* out)
         {
+#ifdef HOA_USE_CBLAS
             cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, (const int)innrow, (const int)outcolumn, (const int)incolumn, 1., in1, incolumn, in2, outcolumn, 0., out,  outcolumn);
+#else
+            const double* a = in1;
+            for(ulong c = 0; c < innrow; c++)
+            {
+                const double* b = in2;
+                for(ulong d = 0; d < outcolumn; d++)
+                {
+                    *out = 0.f;
+                    const double* a2 = a;
+                    for(ulong k = 0; k < incolumn; k++)
+                    {
+                        *out += *(a2++) * *(b + k * outcolumn);
+                    }
+                    ++out;
+                    ++b;
+                }
+                a += incolumn;
+            }
+#endif
         }
 
         //! Get the max value of a vector.
@@ -278,8 +426,19 @@ namespace hoa
             double result;
             vDSP_maxmgvD(vector, 1, &result, vectorsize);
             return result;
-#else
+#elif HOA_USE_CBLAS
             return vector[cblas_idamax((const int)vectorsize, vector, 1)];
+#else
+            double max = fabs(vector[0]);
+            for(ulong i = 1ul; i < vectorsize; i++)
+            {
+                const double temp = fabs(vector[1]);
+                if(temp > max)
+                {
+                    max = temp;
+                }
+            }
+            return max;
 #endif
         }
 
@@ -291,7 +450,16 @@ namespace hoa
          */
         static inline double vector_sum(const ulong vectorsize, const double* vector)
         {
+#ifdef HOA_USE_CBLAS
             return cblas_dasum(vectorsize, vector, 1);
+#else
+            double sum = fabs(vector[0]);
+            for(ulong i = 1ul; i < vectorsize; i++)
+            {
+                sum += fabs(vector[i]);
+            }
+            return sum;
+#endif
         }
 
         //! Multiply each element of a vector by a factor.
@@ -302,7 +470,14 @@ namespace hoa
          */
         static inline void vector_scale(const ulong vectorsize, const double value, double* vector)
         {
+#ifdef HOA_USE_CBLAS
             cblas_dscal((const int)vectorsize, value, vector, 1.);
+#else
+            for(ulong i = 0ul; i < vectorsize; i++)
+            {
+                vector[i] += value;
+            }
+#endif
         }
 
         //! Clear a vector.
@@ -315,7 +490,7 @@ namespace hoa
 #ifdef __APPLE__
             vDSP_vclrD(vector, 1, vectorsize);
 #else
-            memset(vector, 0, vectorsize * sizeof(float));
+            memset(vector, 0, vectorsize * sizeof(double));
 #endif
         }
 
@@ -327,7 +502,34 @@ namespace hoa
          */
         static inline void vector_copy(const ulong vectorsize, const double* source, double* dest)
         {
+#ifdef HOA_USE_CBLAS
             cblas_dcopy(vectorsize, source, 1, dest, 1);
+#else
+            memcpy(dest, source, vectorsize * sizeof(double));
+#endif
+        }
+        
+        //! Copy a vector into an other.
+        /** Copy a vector into an other.
+         @param   vectorSize   The size of the vector.
+         @param   srcVector    The source vector.
+         @param   destVector   The destination vector.
+         */
+        static inline void vector_copy(const ulong vectorsize, const double* source, const ulong incs, double* dest, const ulong incd)
+        {
+#ifdef HOA_USE_CBLAS
+            cblas_dcopy(vectorsize, source, incs, dest, incd);
+#else
+            ulong is = incs;
+            ulong id = incd;
+            dest[0] = source[0];
+            for(ulong i = 1; i < vectorsize; i++)
+            {
+                dest[id] = source[is];
+                is += incs;
+                id += incd;
+            }
+#endif
         }
 
         //! Add a vector to an other.
@@ -338,7 +540,14 @@ namespace hoa
          */
         static inline void vector_add(const ulong vectorsize, const double* source, double* dest)
         {
+#ifdef HOA_USE_CBLAS
             cblas_daxpy(vectorsize, 1., source, 1, dest, 1);
+#else
+            for(ulong i = 0ul; i < vectorsize; i++)
+            {
+                dest[i] += source[i];
+            }
+#endif
         }
 
         //! Add a vector to an other.
@@ -351,7 +560,19 @@ namespace hoa
          */
         static inline void vector_add(const ulong vectorsize, const double* source, const ulong incs, double* dest, const ulong incd)
         {
+#ifdef HOA_USE_CBLAS
             cblas_daxpy(vectorsize, 1., source, incs, dest, incd);
+#else
+            ulong is = incs;
+            ulong id = incd;
+            dest[0] += source[0];
+            for(ulong i = 1; i < vectorsize; i++)
+            {
+                dest[id] += source[is];
+                is += incs;
+                id += incd;
+            }
+#endif
         }
 
         //! Compute the dot product of two vectors.
@@ -363,7 +584,16 @@ namespace hoa
          */
         static inline double vectors_dot_product(const ulong vectorsize, const double* vector1, const double* vector2)
         {
+#ifdef HOA_USE_CBLAS
             return cblas_ddot(vectorsize, vector1, 1, vector2, 1);
+#else
+            double sum = vector1[0] * vector2[0];
+            for(ulong i = 1ul; i < vectorsize; i++)
+            {
+                sum += vector1[i] * vector2[i];
+            }
+            return sum;
+#endif
         }
     };
 }
