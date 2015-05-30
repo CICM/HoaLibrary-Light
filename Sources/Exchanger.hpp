9b67a8049fa4c6a8a4afa0c099627186c9212a3d
pierreguillot <guillotpierre6@gmail.com>
2015-05-30 13:16:23 +0200

    Update exchanger

--- Sources/Exchanger.hpp
+++ Sources/Exchanger.hpp
@@ -190,10 +190,6 @@ namespace hoa
          */
         void process(T const* inputs, T* outputs) noexcept
         {
-            switch(m_normalization)
-            {
-                    
-            }
             switch(m_numbering)
             {
                 case fromFurseMalham:
@@ -207,6 +203,9 @@ namespace hoa
                     break;
                 case toSID:
                     numberToSID(inputs, outputs);
+                    break;
+                default:
+                    Signal<T>::vector_copy(Processor<Hoa2d, T>::Harmonics::getNumberOfHarmonics(), inputs, outputs);
                     break;
             }
         }
@@ -222,12 +221,12 @@ namespace hoa
             *(outputs++) = inputs[0]; // W -> 0
             *(outputs++) = inputs[2]; // Y -> 1
             *(outputs++) = temp;      // X -> 2
-            if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 1ul)
+            if(Processor<Hoa2d, T>::Harmonics::getDecompositionOrder() > 1ul)
             {
                 temp       = inputs[3];
                 *(outputs++) = inputs[4]; // V -> 3
                 *(outputs++) = temp;      // U -> 4
-                if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 2ul)
+                if(Processor<Hoa2d, T>::Harmonics::getDecompositionOrder() > 2ul)
                 {
                     temp       = inputs[5];
                     *(outputs++) = inputs[6]; // Q -> 5
@@ -247,7 +246,7 @@ namespace hoa
             *(outputs++) = inputs[0]; // 0 -> 0
             *(outputs++) = inputs[2]; // 2 -> 1
             *(outputs++) = temp;      // 1 -> 2
-            for(ulong i = 2; i < Processor<Hoa3d, T>::Harmonics::getDecompositionOrder(); i++)
+            for(ulong i = 2; i <= Processor<Hoa2d, T>::Harmonics::getDecompositionOrder(); i++)
             {
                 temp         = inputs[(i-1)*2+1];
                 *(outputs++) = inputs[(i-1)*2+2];
@@ -266,12 +265,12 @@ namespace hoa
             *(outputs++) = inputs[0]; // 0 -> W
             *(outputs++) = inputs[2]; // 2 -> X
             *(outputs++) = temp;      // 1 -> Y
-            if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 1ul)
+            if(Processor<Hoa2d, T>::Harmonics::getDecompositionOrder() > 1ul)
             {
                 temp       = inputs[3];
                 *(outputs++) = inputs[4]; // 4 -> U
                 *(outputs++) = temp;      // 3 -> V
-                if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 2ul)
+                if(Processor<Hoa2d, T>::Harmonics::getDecompositionOrder() > 2ul)
                 {
                     temp       = inputs[5];
                     *(outputs++) = inputs[6]; // 6 -> U
@@ -287,16 +286,17 @@ namespace hoa
          */
         void numberToSID(T const* inputs, T* outputs) noexcept
         {
+            int todo;
             T temp = inputs[1];
             *(outputs++) = inputs[0]; // 0 -> 0
             *(outputs++) = inputs[2]; // 2 -> 1
             *(outputs++) = temp;      // 1 -> 2
-            if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 1ul)
+            if(Processor<Hoa2d, T>::Harmonics::getDecompositionOrder() > 1ul)
             {
                 temp       = inputs[3];
                 *(outputs++) = inputs[4]; // 4 -> 3
                 *(outputs++) = temp;      // 3 -> 4
-                if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 2ul)
+                if(Processor<Hoa2d, T>::Harmonics::getDecompositionOrder() > 2ul)
                 {
                     temp       = inputs[5];
                     *(outputs++) = inputs[6]; // 6 -> 5
@@ -306,7 +306,7 @@ namespace hoa
         }
     };
     
-    template <typename T> class Exchanger<Hoa3d, T> : public Processor<Hoa2d, T>::Harmonics
+    template <typename T> class Exchanger<Hoa3d, T> : public Processor<Hoa3d, T>::Harmonics
     {
     public:
         //! The numbering conversion.
@@ -327,13 +327,14 @@ namespace hoa
         enum Normalization
         {
             SN3D            = 0, /*!<  The normalization is considered as semi-normalization. */
-            fromN3D         = 0, /*!<  From N3D to SN2D. */
-            fromMaxN        = 1, /*!<  From MaxN (B-format) to SN3D. */
-            toN3D           = 2, /*!<  To N3D from SN3D. */
-            toMaxN          = 3  /*!<  To MaxN (B-format) from SN3D. */
+            fromN3D         = 1, /*!<  From N3D to SN2D. */
+            fromMaxN        = 2, /*!<  From MaxN (B-format) to SN3D. */
+            toN3D           = 3, /*!<  To N3D from SN3D. */
+            toMaxN          = 4  /*!<  To MaxN (B-format) from SN3D. */
         };
         
     private:
+        
         Numbering       m_numbering;
         Normalization   m_normalization;
     public:
@@ -342,7 +343,7 @@ namespace hoa
         /**	The exchanger constructor allocates and initialize the member values to renumber and normalize the harmonics channels. The order must be at least 1 and should be 3 at maximum.
          @param     order	The order.
          */
-        inline Exchanger(const ulong order) noexcept : Processor<Hoa2d, T>::Harmonics(order),
+        inline Exchanger(const ulong order) noexcept : Processor<Hoa3d, T>::Harmonics(order),
         m_numbering(ACN),
         m_normalization(SN3D)
         {
@@ -373,6 +374,24 @@ namespace hoa
         {
             m_numbering = toFurseMalham;
             m_normalization = toMaxN;
+        }
+        
+        //! Sets the numbering and the normalization conversion from B-Format.
+        /**	This method the numbering and the normalization conversion from B-Format. Similar to from Furse-Malham numebring and from MaxN normalization.
+         */
+        inline void setFromDaniel() noexcept
+        {
+            m_numbering = fromSID;
+            m_normalization = fromN3D;
+        }
+        
+        //! Sets the numbering and the normalization conversion to B-Format.
+        /**	This method the numbering and the normalization conversion from B-Format. Similar to to Furse-Malham numebring and to MaxN normalization.
+         */
+        inline void setToDaniel() noexcept
+        {
+            m_numbering = toSID;
+            m_normalization = toN3D;
         }
         
         //! Sets the numbering conversion.
@@ -418,24 +437,42 @@ namespace hoa
          */
         void process(T const* inputs, T* outputs) noexcept
         {
+            if(m_numbering == fromFurseMalham)
+            {
+                numberFromFurseMalham(inputs, outputs);
+            }
+            else if(m_numbering == fromSID)
+            {
+                numberFromSID(inputs, outputs);
+            }
+            else
+            {
+                Signal<T>::vector_copy(Processor<Hoa3d, T>::Harmonics::getNumberOfHarmonics(), inputs, outputs);
+            }
             switch(m_normalization)
             {
-                    
+                case fromMaxN:
+                    normalizeFromMaxN(outputs, outputs);
+                    break;
+                case fromN3D:
+                    normalizeFromN3D(outputs, outputs);
+                    break;
+                case toMaxN:
+                    normalizeToMaxN(outputs, outputs);
+                    break;
+                case toN3D:
+                    normalizeToN3D(outputs, outputs);
+                    break;
+                default:
+                    break;
             }
-            switch(m_numbering)
+            if(m_numbering == toFurseMalham)
             {
-                case fromFurseMalham:
-                    numberFromFurseMalham(inputs, outputs);
-                    break;
-                case fromSID:
-                    numberFromSID(inputs, outputs);
-                    break;
-                case toFurseMalham:
-                    numberToFurseMalham(inputs, outputs);
-                    break;
-                case toSID:
-                    numberToSID(inputs, outputs);
-                    break;
+                numberToFurseMalham(outputs, outputs);
+            }
+            else if(m_numbering == toSID)
+            {
+                numberToSID(outputs, outputs);
             }
         }
         
@@ -466,7 +503,7 @@ namespace hoa
                     temp      = inputs[9];
                     temp2     = inputs[10];
                     temp3     = inputs[12];
-                    T temp4   = inputs[14];
+                    const T temp4   = inputs[14];
                     outputs[9]  = inputs[15];// Q(15) -> 9
                     outputs[10] = inputs[13];// O(13) -> 10
                     outputs[11] = inputs[11];// M(11) -> 11
@@ -474,6 +511,51 @@ namespace hoa
                     outputs[13] = temp2;     // L(10) -> 13
                     outputs[14] = temp3;     // N(12) -> 14
                     outputs[15] = temp4;     // P(14) -> 15
+                    if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 3ul)
+                    {
+                        Signal<T>::vector_copy(Processor<Hoa3d, T>::Harmonics::getNumberOfHarmonics() - 16ul, inputs+16, outputs+16);
+                    }
+                }
+            }
+        }
+        
+        //! This method number the channels from ACN to Furse-Malham.
+        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs array and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
+         @param     inputs   The inputs array.
+         @param     outputs  The outputs array.
+         */
+        void numberToFurseMalham(T const* inputs, T* outputs) noexcept
+        {
+            T temp  = inputs[1];
+            T temp2 = inputs[2];
+            outputs[0] = inputs[0]; // 0 -> W(0)
+            outputs[1] = inputs[3]; // 3 -> X(1)
+            outputs[2] = temp;      // 1 -> Y(2)
+            outputs[3] = temp2;     // 2 -> Z(3)
+            if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 1ul)
+            {
+                temp       = inputs[4];
+                temp2      = inputs[5];
+                outputs[4] = inputs[6]; // 6 -> R(4)
+                outputs[5] = inputs[7]; // 7 -> S(5)
+                outputs[6] = temp2;     // 5 -> T(6)
+                outputs[7] = inputs[8]; // 8 -> U(7)
+                outputs[8] = temp;      // 4 -> V(8)
+                if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 2ul)
+                {
+                    temp            = inputs[9];
+                    temp2           = inputs[10];
+                    outputs[9]  = inputs[12];// 12 -> K(9)
+                    outputs[10] = inputs[13];// 13 -> L(10)
+                    outputs[11] = inputs[11];// 11 -> M(11)
+                    outputs[12] = inputs[14];// 14 -> N(12)
+                    outputs[13] = temp2;     // 10 -> 0(13)
+                    outputs[14] = inputs[15];// 15 -> P(14)
+                    outputs[15] = temp;      // 9  -> Q(15)
+                    if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 3ul)
+                    {
+                        Signal<T>::vector_copy(Processor<Hoa3d, T>::Harmonics::getNumberOfHarmonics() - 16ul, inputs+16, outputs+16);
+                    }
                 }
             }
         }
@@ -485,6 +567,7 @@ namespace hoa
          */
         void numberFromSID(T const* inputs, T* outputs) noexcept
         {
+            int todo;
             T temp = inputs[1];
             *(outputs++) = inputs[0]; // 0 -> 0
             *(outputs++) = inputs[2]; // 2 -> 1
@@ -501,31 +584,6 @@ namespace hoa
             }
         }
         
-        //! This method number the channels from ACN to Furse-Malham.
-        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs array and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
-         @param     inputs   The inputs array.
-         @param     outputs  The outputs array.
-         */
-        void numberToFurseMalham(T const* inputs, T* outputs) noexcept
-        {
-            T temp = inputs[1];
-            *(outputs++) = inputs[0]; // 0 -> W
-            *(outputs++) = inputs[2]; // 2 -> X
-            *(outputs++) = temp;      // 1 -> Y
-            if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 1ul)
-            {
-                T temp       = inputs[3];
-                *(outputs++) = inputs[4]; // 4 -> U
-                *(outputs++) = temp;      // 3 -> V
-                if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 2ul)
-                {
-                    T temp       = inputs[5];
-                    *(outputs++) = inputs[6]; // 6 -> U
-                    *(outputs++) = temp;      // 5 -> Q
-                }
-            }
-        }
-        
         //! This method number the channels from ACN to SID.
         /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs array and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
          @param     inputs   The inputs array.
@@ -533,6 +591,7 @@ namespace hoa
          */
         void numberToSID(T const* inputs, T* outputs) noexcept
         {
+            int todo;
             T temp = inputs[1];
             *(outputs++) = inputs[0]; // 0 -> W
             *(outputs++) = inputs[2]; // 2 -> 1
@@ -547,6 +606,112 @@ namespace hoa
                     T temp       = inputs[5];
                     *(outputs++) = inputs[6]; // 6 -> 5
                     *(outputs++) = temp;      // 5 -> 6
+                }
+            }
+        }
+        
+        //! This method normalizes the channels from MaxN to SN3D.
+        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs array and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
+         @param     inputs   The inputs array.
+         @param     outputs  The outputs array.
+         */
+        void normalizeFromMaxN(T const* inputs, T* outputs) noexcept
+        {
+            outputs[0] = inputs[0] * sqrt(2.);
+            outputs[1] = inputs[1] * sqrt(3.);
+            outputs[2] = inputs[2] * sqrt(3.);
+            outputs[3] = inputs[3] * sqrt(3.);
+            if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 1ul)
+            {
+                outputs[4] = inputs[4] * (sqrt(15.) / 2.);
+                outputs[5] = inputs[5] * (sqrt(15.) / 2.);
+                outputs[6] = inputs[6] * sqrt(5.);
+                outputs[7] = inputs[7] * (sqrt(15.) / 2.);
+                outputs[8] = inputs[8] * (sqrt(15.) / 2.);
+                if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 2ul)
+                {
+                    outputs[9]  = inputs[9]  * sqrt(35. / 8.);
+                    outputs[10] = inputs[10] * (sqrt(35.) / 3.);
+                    outputs[11] = inputs[11] * sqrt(224. / 45);
+                    outputs[12] = inputs[12] * sqrt(7.);
+                    outputs[13] = inputs[13] * sqrt(224. / 45);
+                    outputs[14] = inputs[14] * (sqrt(35.) / 3.);
+                    outputs[15] = inputs[15] * sqrt(35. / 8.);
+                }
+            }
+        }
+        
+        //! This method normalizes the channels from SN3D to MaxN.
+        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs array and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
+         @param     inputs   The inputs array.
+         @param     outputs  The outputs array.
+         */
+        void normalizeToMaxN(T const* inputs, T* outputs) noexcept
+        {
+            outputs[0] = inputs[0] / sqrt(2.);
+            outputs[1] = inputs[1] / sqrt(3.);
+            outputs[2] = inputs[2] / sqrt(3.);
+            outputs[3] = inputs[3] / sqrt(3.);
+            if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 1ul)
+            {
+                outputs[4] = inputs[4] / (sqrt(15.) / 2.);
+                outputs[5] = inputs[5] / (sqrt(15.) / 2.);
+                outputs[6] = inputs[6] / sqrt(5.);
+                outputs[7] = inputs[7] / (sqrt(15.) / 2.);
+                outputs[8] = inputs[8] / (sqrt(15.) / 2.);
+                if(Processor<Hoa3d, T>::Harmonics::getDecompositionOrder() > 2ul)
+                {
+                    outputs[9]  = inputs[9]  / sqrt(35. / 8.);
+                    outputs[10] = inputs[10] / (sqrt(35.) / 3.);
+                    outputs[11] = inputs[11] / sqrt(224. / 45);
+                    outputs[12] = inputs[12] / sqrt(7.);
+                    outputs[13] = inputs[13] / sqrt(224. / 45);
+                    outputs[14] = inputs[14] / (sqrt(35.) / 3.);
+                    outputs[15] = inputs[15] / sqrt(35. / 8.);
+                }
+            }
+        }
+        
+        //! This method normalizes the channels from N3D to SN3D.
+        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs array and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
+         @param     inputs   The inputs array.
+         @param     outputs  The outputs array.
+         */
+        void normalizeFromN3D(T const* inputs, T* outputs) noexcept
+        {
+            T norm = sqrt(3.);
+            *(outputs++) = *(inputs++);
+            *(outputs++) = *(inputs++) * norm;
+            *(outputs++) = *(inputs++) * norm;
+            *(outputs++) = *(inputs++) * norm;
+            for(ulong i = 2; i <= Processor<Hoa3d, T>::Harmonics::getDecompositionOrder(); i++)
+            {
+                norm = sqrt(2. * T(i) + 1.);
+                for(ulong j = 0; j < i * 2ul + 1ul; j++)
+                {
+                    *(outputs++) = *(inputs++) * norm;
+                }
+            }
+        }
+        
+        //! This method normalizes the channels from SN3D to N3D.
+        /**	You should use this method for in-place or not-in-place processing and sample by sample. The inputs array and outputs array contains the spherical harmonics samples and the minimum size must be the number of harmonics.
+         @param     inputs   The inputs array.
+         @param     outputs  The outputs array.
+         */
+        void normalizeToN3D(T const* inputs, T* outputs) noexcept
+        {
+            T norm = 1. / sqrt(3.);
+            *(outputs++) = *(inputs++);
+            *(outputs++) = *(inputs++) * norm;
+            *(outputs++) = *(inputs++) * norm;
+            *(outputs++) = *(inputs++) * norm;
+            for(ulong i = 2; i <= Processor<Hoa3d, T>::Harmonics::getDecompositionOrder(); i++)
+            {
+                norm = 1. / sqrt(2. * T(i) + 1.);
+                for(ulong j = 0; j < i * 2ul + 1ul; j++)
+                {
+                    *(outputs++) = *(inputs++) * norm;
                 }
             }
         }
