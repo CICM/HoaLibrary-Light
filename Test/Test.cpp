910af2ed4364666b96742cd7fb68b44d6c71ba33
pierreguillot <guillotpierre6@gmail.com>
2016-09-23 21:23:37 +0200

    add cassert to tests

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cassert>
 
 static void test_binaural()
 {
@@ -16,7 +17,7 @@ static void test_binaural()
     const unsigned i_input_nb   = 4;
     const unsigned i_output_nb  = 2;
     const unsigned i_nb_samples = 64000;
-    
+
     srand(time(NULL));
     hoa::Decoder<hoa::Hoa3d, float>::Binaural *decoder = new hoa::Decoder<hoa::Hoa3d, float>::Binaural(i_order);
     float *p_src    = (float *)malloc(i_input_nb * i_nb_samples * sizeof(float));
@@ -27,21 +28,21 @@ static void test_binaural()
     {
         p_src[i] = rand() / RAND_MAX + 1.f;
     }
-    
+
     float **in_buf  = (float **)malloc(i_input_nb * sizeof(float *));
     for (unsigned i = 0; i < i_input_nb; ++i)
     {
         in_buf[i] = (float *)malloc(i_blck_size * sizeof(float));
     }
-    
+
     float **out_buf = (float **)malloc(i_output_nb * sizeof(float *));
     for (unsigned i = 0; i < i_output_nb; ++i)
     {
         out_buf[i] = (float *)malloc(i_blck_size * sizeof(float));
     }
-    
+
     decoder->computeRendering(i_blck_size);
-    
+
     for(unsigned i = 0; i < i_nb_samples; i += i_blck_size)
     {
         for(unsigned j = 0; j < i_input_nb; ++j)
@@ -51,9 +52,9 @@ static void test_binaural()
                 in_buf[j][i] = p_src[(i + k) * i_input_nb + j];
             }
         }
-        
+
         decoder->processBlock((const float**)in_buf, (float**)out_buf);
-        
+
         for(unsigned j = 0; j < i_output_nb; ++j)
         {
             for(unsigned k = 0; k < i_blck_size; ++k)
@@ -62,28 +63,28 @@ static void test_binaural()
             }
         }
     }
-    
-    
+
+
     for(unsigned i = 0; i < i_output_nb * i_nb_samples; ++i)
     {
         assert(p_dest[i] != 0.f && "problem ici");
     }
-    
+
     for(unsigned i = 0; i < i_output_nb; ++i)
     {
         free(out_buf[i]);
     }
     free(out_buf);
-    
+
     for(unsigned i = 0; i < i_input_nb; ++i)
     {
         free(in_buf[i]);
     }
     free(in_buf);
-    
+
     free(p_src);
     free(p_dest);
-    
+
     delete decoder;
 }
 
