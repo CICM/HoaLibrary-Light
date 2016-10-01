5626ef40e86d5fd349d8207dff91c488022b4714
pierreguillot <guillotpierre6@gmail.com>
2016-10-01 09:19:55 +0200

    3

--- Sources/Hoa_Encoder.hpp
+++ Sources/Hoa_Encoder.hpp
@@ -75,8 +75,8 @@ namespace hoa
             order = order >= 1 ? order : 1;
             size_t const nharmo = Harmonic<Hoa3d, T>::getNumberOfHarmonics(order);
             m_radius_coeffs         = new T[order + 1];
-            //m_azimuth_coeffs        = new T[order * 2 + 1];
-            //m_elevation_coeffs      = new T[nharmo / 2 + (order + 1)];
+            m_azimuth_coeffs        = new T[order * 2 + 1];
+            m_elevation_coeffs      = new T[nharmo / 2 + (order + 1)];
             m_normalization_coeffs  = new T[nharmo];
             setRadius(1.);
             //setAzimuth(0.);
@@ -91,10 +91,12 @@ namespace hoa
         //! The destructor.
         ~Encoder() hoa_noexcept
         {
+            /*
             delete [] m_radius_coeffs;
             delete [] m_azimuth_coeffs;
             delete [] m_elevation_coeffs;
             delete [] m_normalization_coeffs;
+             */
         }
         
         
