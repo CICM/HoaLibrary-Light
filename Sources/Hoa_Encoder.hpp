d0a116f297ac05dbcdc17045b39b4e1b592ae63d
pierreguillot <guillotpierre6@gmail.com>
2016-10-01 09:29:10 +0200

    encoder+4

--- Sources/Hoa_Encoder.hpp
+++ Sources/Hoa_Encoder.hpp
@@ -72,16 +72,15 @@ namespace hoa
         //! @param order The order of decomposition.
         Encoder(size_t order) : ProcessorHarmonics<D, T>(order)
         {
-            order = order >= 1 ? order : 1;
-            size_t const nharmo = Harmonic<Hoa3d, T>::getNumberOfHarmonics(order);
-
-            m_radius_coeffs         = new T[ProcessorHarmonics<D, T>::getDecompositionOrder() + 1];
-            m_azimuth_coeffs        = new T[ProcessorHarmonics<D, T>::getDecompositionOrder() * 2 + 1];
+            order = ProcessorHarmonics<D, T>::getDecompositionOrder() >= 1 ? ProcessorHarmonics<D, T>::getDecompositionOrder() : 1;
+            
+            m_radius_coeffs         = new T[order+ 1];
+            m_azimuth_coeffs        = new T[order * 2 + 1];
+            m_elevation_coeffs      = new T[((order + 1) * (order + 1)) / 2 + (order + 1)];
             /*
-            m_elevation_coeffs      = new T[nharmo / 2 + (order + 1)];
             m_normalization_coeffs  = new T[nharmo];
             
-            setElevation(0.);
+            
             
             for(size_t i = 0; i < nharmo; ++i)
             {
@@ -90,6 +89,7 @@ namespace hoa
              */
             setRadius(1.);
             setAzimuth(0.);
+            setElevation(0.);
         }
 
         //! The destructor.
